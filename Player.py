2e12d188c438946ba0d5cbed685c328bab3bab2e
kkoroev <kkoroev@dengabank.ru>
2022-01-20 15:52:30 +0300

    Make the 'Weapon' class abstract, refactoring by PEP8

--- Player.py
+++ Player.py
@@ -1,24 +1,28 @@
-import pygame
 import math
+
+import pygame
+
 import Weapon
 
 PLAYERCOLOR = (255,   0,   0)
 
+
 def normalize_vector(vector):
     if vector == [0, 0]:
-        return [0, 0]    
+        return [0, 0]
     pythagoras = math.sqrt(vector[0]*vector[0] + vector[1]*vector[1])
-    return (vector[0] / pythagoras, vector[1] / pythagoras)
+    return vector[0] / pythagoras, vector[1] / pythagoras
+
 
 class Player(pygame.sprite.Sprite):
     projectiles = pygame.sprite.Group()
+
     def __init__(self, screenSize):
         super().__init__()
         self.image = pygame.Surface([8, 8])
         self.image.fill(PLAYERCOLOR)
-        self.rect = self.image.get_rect(x=screenSize[0]//2,
-                                        y=screenSize[1]//2)
-        
+        self.rect = self.image.get_rect(x=screenSize[0]//2, y=screenSize[1]//2)
+
         self.pos = [screenSize[0] // 2, screenSize[1] // 2]
         self.health = 3
         self.alive = True
@@ -46,12 +50,12 @@ class Player(pygame.sprite.Sprite):
             self.pos[1] = screenSize[1]-self.rect.width
         else:
             self.pos[1] = newPos[1]
-        
+
         self.rect.topleft = self.pos
         self.movementVector = [0, 0]
-        
+
     def shoot(self, mousePos):
         self.equippedWeapon.shoot(self, mousePos)
-        
+
     def render(self, surface):
         surface.blit(self.image, self.pos)
